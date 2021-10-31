44525c353fdcf5ee41245fc7ad3af9847a7e021f
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-10-30 21:12:13 -0600

    plots

--- plots.R
+++ plots.R
@@ -17,128 +17,264 @@ library(dplyr)
 
 data=read.csv("data/channelform.csv")
 attach(data)
-data
-summary(data)
+
+
+# Elevation ---------------------------------------------------------------
+
 
 elevation_data <- data %>% filter(variable=='elevation')
 
-E <- ggplot(elevation_data, aes(x=streams, y=value)) + 
-  labs(title="Elevacion",x="Forma", y = "Elevacion")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+elevation_data$streams <- factor(elevation_data$streams,
+              levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
 
+
+E <- ggplot(elevation_data, aes(x=streams, y=value, fill=streams)) + 
+  labs(title="",x="", y = "Stream elevation (masl)")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+  
+   
+E
+
+# Stream wide -------------------------------------------------------------
 
 wide_data <- data %>% filter(variable=='wide')
 wide_data1 <- na.omit(wide_data)
 
-W <- ggplot(wide_data1, aes(x=streams, y=value)) + 
-  labs(title="Wide", x="Forma", y = "Channel width")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+wide_data1$streams <- factor(wide_data1$streams,
+                    levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
 
+W <- ggplot(wide_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="", y = "Channel width (m)")+
+
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+
+W
+
+
+# Water velocity ----------------------------------------------------------
 
 velocity_data <- data %>% filter(variable=='velocity')
 velocity_data1 <- na.omit(velocity_data)
 
-V <- ggplot(velocity_data1, aes(x=streams, y=value)) + 
-  labs(title="velocity", x="Forma", y = "Velocity")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+velocity_data1$streams <- factor(velocity_data1$streams,
+                             levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
+
+V <- ggplot(velocity_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="", y = "Velocity (m/s)")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+
+V
 
 
+# Boulder % -----------------------------------------------------------------
 
-rocks_data <- data %>% filter(variable=='rocks')
-rocks_data1 <- na.omit(rocks_data)
+boulder_data <- data %>% filter(variable=='Boulder')
+boulder_data1 <- na.omit(boulder_data)
 
-R <- ggplot(rocks_data1, aes(x=streams, y=value)) + 
-  labs(title="Boulder", x="Forma", y = "Boulder")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+boulder_data1$streams <- factor(boulder_data1$streams,
+                                 levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
+
+B <- ggplot(boulder_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="", y = "Boulder")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+
+B
 
 
-canto_data <- data %>% filter(variable=='canto')
-canto_data1 <- na.omit(canto_data)
+# Cobble % ----------------------------------------------------------------
 
-C <- ggplot(canto_data1, aes(x=streams, y=value)) + 
-  labs(title="Canto", x="Forma", y = "Canto")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+cobble_data <- data %>% filter(variable=='Cobble')
+cobble_data1 <- na.omit(cobble_data)
 
-grava_data <- data %>% filter(variable=='grava')
-grava_data1 <- na.omit(grava_data)
+cobble_data1$streams <- factor(cobble_data1$streams,
+                    levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
 
-G <- ggplot(grava_data1, aes(x=streams, y=value)) + 
-  labs(title="Grava", x="Forma", y = "Grava")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+C <- ggplot(cobble_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="", y = "Cobble")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+
+C
+
+
+# Gravel ------------------------------------------------------------------
+
+
+gravel_data <- data %>% filter(variable=='Gravel')
+gravel_data1 <- na.omit(gravel_data)
+
+gravel_data1$streams <- factor(gravel_data1$streams,
+                               levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
+
+G <- ggplot(gravel_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="Channel form", y = "Gravel")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5) +
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+
+G
+
+
+# Sand --------------------------------------------------------------------
+
 
 sand_data <- data %>% filter(variable=='sand')
 sand_data1 <- na.omit(sand_data)
 
-S <- ggplot(sand_data1, aes(x=streams, y=value)) + 
-  labs(title="Sand", x="Forma", y = "Sand")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+sand_data1$streams <- factor(sand_data1$streams,
+                               levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
+
+S <- ggplot(sand_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="Channel form", y = "Sand")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
+
+S
+
+
+# Silt --------------------------------------------------------------------
 
 silt_data <- data %>% filter(variable=='silt')
 silt_data1 <- na.omit(silt_data)
 
-Si <- ggplot(silt_data1, aes(x=streams, y=value)) + 
-  labs(title="Silt", x="Forma", y = "Silt")+
-  geom_violin() +  
-  geom_jitter(shape=16, position=position_jitter(0.2))+ 
-  theme_classic()
+silt_data1$streams <- factor(silt_data1$streams,
+                             levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
 
-E+W+V+R+C+G+S+Si
+Si <- ggplot(silt_data1, aes(x=streams, y=value,  fill=streams)) + 
+  labs(title="", x="Channel form", y = "Silt")+
+  
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  geom_violin()+
+  geom_jitter(width=0.1,alpha=0.5)+
+  theme_classic()+
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black"))  #subaxis y
 
-R+C+G+S+Si
-
-
-
-head(data)
-
-b <- select(data, -streams)
-summary(b)
-
-c <- b%>%
-pivot_wider(names_from = variable, values_from = value)
-
-d <- select(c, -count)
-summary(d)
-
-res.comp <- imputePCA(d, ncp=2, scale = TRUE, method = c("Regularized","EM"), 
-                      row.w = NULL, ind.sup=NULL,quanti.sup=NULL,quali.sup=NULL,
-                      coeff.ridge = 1, threshold = 1e-06, seed = NULL, nb.init = 1,  
-                      maxiter = 1000)
+Si
 
 
-canal <- prcomp(res.comp$completeObs, center = TRUE, scale = TRUE)
-summary(canal)
 
-autoplot(canal,loadings = TRUE, loadings.colour = 'blue',
-         loadings.label = TRUE, loadings.label.size = 3,data=d)
+Fig <- (E+W+V+B+C+G+S+Si) + plot_annotation(tag_levels = 'A')
+Fig
 
-
-res.var <- get_pca_var(canal)
-res.var$contrib  
+Fig + ggsave("Figure 1.jpg",width = 8.5, height = 11, units = "in", dpi=300)
 
 
-label=read.csv("label.csv")
-head(label)
 
-fviz_pca_var(canal,
-             col.var = "contrib", # Color by contributions to the PC
-             repel = TRUE)     # Avoid text overlapping
+library(tidyverse)
+# First, rename variables
+sampling_new <- c(
+  'elevation' = "Elevation",
+  'wide' = "Channel wide",
+  'velocity' = "Water velocity",
+  'Boulder' = "Boulder",
+  'Cobble' = "Cobble",
+  'Gravel' = "Gravel",
+  'sand' = "Sand",
+  'silt' = "Silt")
 
-fviz_pca_biplot(canal, 
-                col.ind = label$streams, palette = "jco", 
-                addEllipses = TRUE, label = "var",
-                col.var = "black", repel = TRUE,
-                legend.title = "Chanel form")
+
+data$variable = factor(data$variable, levels=c('elevation','wide','velocity',
+                        'Boulder', "Cobble","Gravel", "sand", "silt"))
+
+data$streams <- factor(data$streams,
+                levels = c("V-shape", "Trapezoid","U-shape"),ordered = TRUE)
+
+p <-  ggplot(data, aes(x = streams, y = value, fill = streams))+
+  geom_violin() + 
+  geom_jitter(width=0.1,alpha=0.5) +
+  labs(title="", x="Channel form", y = "Value") +
+  
+  theme_bw() +
+  scale_fill_manual(values=c("#7570b3", "#d95f02", "#1f78b4")) +
+  theme(legend.position="none") +
+  
+  theme(axis.title.x = element_text(size = 12, angle = 0)) + # axis x
+  theme(axis.title.y = element_text(size = 12, angle = 90)) + # axis y
+  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis x
+  theme(axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) + #subaxis y
+
+   facet_wrap(variable~., scales="free", labeller = labeller(variable=c(
+  'elevation' = "Stream elevation (masl)",
+  'wide' = "Channel wide (m)",
+  'velocity' = "Water velocity (m/s)",
+  'Boulder' = "Boulder (%)",
+  'Cobble' = "Cobble (%)",
+  'Gravel' = "Gravel (%)",
+  'sand' = "Sand (%)",
+  'silt' = "Silt (%)")))
+p    
+
+p  + ggsave("Figure 2.jpg",width = 8.5, height = 11, units = "in", dpi=300)
+
+
+
